5c1166a0bec45e022625acaa2f73dd0935556210
Adi Tangidy <adi.tangidy@gmail.com>
2018-12-03 00:48:09 +0800

    CORS issue

--- src/main/java/com/vee/demo/todoservice/controller/ToDoController.java
+++ src/main/java/com/vee/demo/todoservice/controller/ToDoController.java
@@ -3,6 +3,7 @@ package com.vee.demo.todoservice.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ public class ToDoController {
 	private ToDoService todoService;
 
 	@GetMapping("/list")
+	@CrossOrigin(origins="http://localhost:4200")
 	public List<ToDo> listTodo(){
 		return todoService.list();
 	} 
